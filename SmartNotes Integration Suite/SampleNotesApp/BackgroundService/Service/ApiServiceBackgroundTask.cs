9c3d88f4f8874b161e49eee0a45c5e13da4f24fa
mdplabon <mdplabonda@gmail.com>
2024-12-10 20:38:08 +0600

    Added sample app

---
+++ SmartNotes Integration Suite/SampleNotesApp/BackgroundService/Service/ApiServiceBackgroundTask.cs
@@ -0,0 +1,32 @@
+﻿using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+
+namespace BackgroundService.Service
+{
+    public sealed class ApiServiceBackgroundTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral _deferral;
+        private AppServiceConnection _appServiceConnection;
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            _deferral = taskInstance.GetDeferral();
+            var details = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+            _appServiceConnection = details?.AppServiceConnection;
+            if (_appServiceConnection != null)
+            {
+                _appServiceConnection.RequestReceived += OnRequestReceived;
+                _appServiceConnection.ServiceClosed += OnServiceClosed;
+            }
+        }
+
+        private async void OnRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            //
+        }
+
+        private void OnServiceClosed(AppServiceConnection sender, AppServiceClosedEventArgs args)
+        {
+            _deferral.Complete();
+        }
+    }
+}
